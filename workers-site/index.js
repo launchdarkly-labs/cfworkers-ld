b28cc5987ae6c900cccde71b5e7d9fe6c38950f5
Brian Rinaldi <brian.rinaldi@gmail.com>
2021-12-16 15:21:17 -0500

    cleanup

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,17 +1,5 @@
-import {
-  getAssetFromKV,
-  mapRequestToAsset,
-} from "@cloudflare/kv-asset-handler";
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 const { init } = require("launchdarkly-cloudflare-edge-sdk");
-
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false;
 
 class FlagsStateInjector {
   async element(element) {
@@ -28,14 +16,15 @@ class FlagsStateInjector {
   }
 }
 
-class ElementHandler {
+class H1ElementHandler {
   async element(element) {
+    // replace the header text with the value of a string flag
     const headerText = await getFlagValue("header-text");
     element.setInnerContent(headerText);
   }
 }
 const rewriter = new HTMLRewriter();
-rewriter.on("h1", new ElementHandler());
+rewriter.on("h1", new H1ElementHandler());
 rewriter.on("head", new FlagsStateInjector());
 let ldClient;
 
@@ -57,20 +46,7 @@ async function getFlagValue(key, user) {
 async function handleEvent(event) {
   let options = {};
 
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
-
   try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      };
-    }
-
     if (!ldClient) {
       ldClient = init(MY_KV, "61409b046ca8d52601d179ef");
       await ldClient.waitForInitialization();
@@ -85,26 +61,10 @@ async function handleEvent(event) {
     response.headers.set("X-Content-Type-Options", "nosniff");
     response.headers.set("X-Frame-Options", "DENY");
     response.headers.set("Referrer-Policy", "unsafe-url");
-    response.headers.set("Feature-Policy", "none");
 
     return rewriter.transform(response);
   } catch (e) {
     console.log(e);
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: (req) =>
-            new Request(`${new URL(req.url).origin}/404.html`, req),
-        });
-
-        return new Response(notFoundResponse.body, {
-          ...notFoundResponse,
-          status: 404,
-        });
-      } catch (e) {}
-    }
-
     return new Response(e.message || e.toString(), { status: 500 });
   }
 }
