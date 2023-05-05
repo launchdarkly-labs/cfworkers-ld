ed1e43d16aeb412033620e1c55000e669913b2db
Yusinto Ngadiman <yusinto@gmail.com>
2023-05-05 23:50:10 +0800

    chore:initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,28 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-import { init, LDClient } from "launchdarkly-cloudflare-edge-sdk";
+import { init as initLD, LDClient } from '@launchdarkly/cloudflare-server-sdk';
+import { ExecutionContext, KVNamespace, Request } from '@cloudflare/workers-types';
 
-// Declare MY_KV as a global variable
-declare global {
-  var MY_KV: KVNamespace;
+interface Bindings {
+  LD_KV: KVNamespace;
 }
 
 let ldClient: LDClient;
 
-class FlagsStateInjector {
-  async element(element: Element) {
-      // fetch all flag values for client-side SDKs 
-      // as evaluated for a context that has not logged in
-      // use a more appropriate context key if needed
-    const context = { key: 'guest' };
-    const allFlags = await ldClient.allFlagsState(context, {
-      clientSideOnly: true,
-    });
-    element.append(
-      `<script>window.ldFlags = ${JSON.stringify(allFlags)}</script>`,
-      { html: true }
-    );
-  }
-}
+export default {
+  async fetch(request: Request, env: Bindings, ctx: ExecutionContext): Promise<Response> {
+    const sdkKey = 'test-sdk-key';
+    const flagKey = 'testFlag1';
+    const context = { kind: 'user', key: 'test-user-key-1', email: 'test@gmail.com' };
 
-class H1ElementHandler {
-  async element(element: Element) {
-    // replace the header text with the value of a string flag
-    const headerText = await getFlagValue("header-text");
-    element.setInnerContent(headerText);
-  }
-}
-
-const rewriter = new HTMLRewriter();
-rewriter.on("head", new FlagsStateInjector());
-rewriter.on("h1", new H1ElementHandler());
-
-async function getFlagValue(key: string, context?) {
-  let flagValue;
-  if (!context) {
-    context = {
-      key: 'guest',
-    };
-  }
-  flagValue = await ldClient.variation(key, context, false);
-  return flagValue;
-}
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleEvent(event));
-});
-
-async function handleEvent(event: FetchEvent) {
-  if (!ldClient) {
-    ldClient = init(MY_KV, "63455fcfeffc3d114a12c3bd", {});
+    ldClient = initLD(sdkKey, env.LD_KV);
     await ldClient.waitForInitialization();
-  }
+    const flagValue = await ldClient.variation(flagKey, context, false);
+    const flagDetail = await ldClient.variationDetail(flagKey, context, false);
+    const allFlags = await ldClient.allFlagsState(context);
 
-  let options = {};
-  try {
-    const page = await getAssetFromKV(event, options);
-    const response = new Response(page.body, page);
-
-    const customHeader = await getFlagValue("custom-response-headers");
-    customHeader.headers.forEach((header: { name: string; value: string }) => {
-      response.headers.set(header.name, header.value);
-    });
-
-    return rewriter.transform(response);
-  } catch (e: any) {
-    console.log(e);
-    return new Response(e.message || e.toString(), { status: 500 });
-  }
-}
+    const resp = `
+    ${flagKey}: ${flagValue}
+    detail: ${JSON.stringify(flagDetail)}
+    allFlags: ${JSON.stringify(allFlags)}`;
+    return new Response(`${resp}`);
+  },
+};
